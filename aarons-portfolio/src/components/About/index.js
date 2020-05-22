f0612c51d741e2b4e6b9f38f80789ceb662ae249
Aaron Thomas <acthomas83@gmail.com>
2020-05-22 01:13:51 -0400

    components

---
+++ aarons-portfolio/src/components/About/index.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <div id="section1" class="center" class="container-fluid bg-success">
+    <div class="container-fluid center" id="background-animation">
+      <img class="center" id="logo" src="./images/aaron-thomas-logo-2.png" alt="Aaron Thomas Logo"></img>
+      <img class="center" id="logo-sm" src="./images/aaron-thomas-logo-2-sm.png" alt="Aaron Thomas Logo"></img>
+    </div>
+  </div>
+  <div id="pink"></div>
+  <div id="section2" class="container-fluid">
+    <h1>Aaron Thomas</h1>
+    <h5>Full Stack Developer</h5>
+    <p>
+      <img id="profile-picture" src="../src/images/aaron-profile-drk.png" alt="Aaron's Photo"></img>
+      <p class="text">
+        Since the age of 9, I was drawn to computers and technology in general. As the years passed, I
+        became
+        very
+        proficient using computers, particularly my Macintosh Performa 635. Before long, I began to wonder
+        how
+        it
+        all works. With that curiosity, I found myself completely disassembling my computer and miraculously
+        putting
+        it all back together with no help at all. YouTube University wasn’t around back then. After many
+        more
+        years
+        of self-educating myself on the basics of Information Technology, I landed my first IT job as a
+        Technical
+        Support Specialist at Philips. With so much exposure to technology and vital company systems, I knew
+        that I
+        needed to expand my technical knowledge through formal education. After reading about the Coding
+        Boot
+        Camp,
+        I knew this would be the opportunity I’ve been waiting for to help me connect the many “technical
+        dots”
+        that
+        I have collected over the years.
+        <br></br> <br></br>
+        Since the age of 9, I was drawn to computers and technology in general. As the years passed, I
+        became
+        very
+        proficient using computers, particularly my Macintosh Performa 635. Before long, I began to wonder
+        how
+        it
+        all works. With that curiosity, I found myself completely disassembling my computer and miraculously
+        putting
+        it all back together with no help at all. YouTube University wasn’t around back then. After many
+        more
+        years
+        of self-educating myself on the basics of Information Technology, I landed my first IT job as a
+        Technical
+        Support Specialist at Philips. With so much exposure to technology and vital company systems, I knew
+        that I
+        needed to expand my technical knowledge through formal education. After reading about the Coding
+        Boot
+        Camp,
+        I knew this would be the opportunity I’ve been waiting for to help me connect the many “technical
+        dots”
+        that
+        I have collected over the years.
+      </p>
+    </p>
+  </div>
+  <div class="center" id="orange">
+        <div id="download">
+      Click <a href="./assets/aaron-thomas-resume-2020.pdf">HERE</a> to download my personal resume here </div>
+        </div>        
+    </div>
+    );
+  }
+}
+
+export default About;
