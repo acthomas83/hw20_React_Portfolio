64c61a3f227e31b6c5502c3f75d7aedeb2e61529
Aaron Thomas <acthomas83@gmail.com>
2020-05-23 00:13:42 -0400

    Updates

---
+++ precache-manifest.b0d70b64fe5c78870bcc11f0d23b6909.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "870025297b362c2c8f37184431ea2e0d",
+    "url": "/hw20_React_Portfolio/index.html"
+  },
+  {
+    "revision": "a1392cfd1ca4e2439796",
+    "url": "/hw20_React_Portfolio/static/css/main.87b0cc7f.chunk.css"
+  },
+  {
+    "revision": "8c1de296d5cd780dabbb",
+    "url": "/hw20_React_Portfolio/static/js/2.ad0eb749.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw20_React_Portfolio/static/js/2.ad0eb749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1392cfd1ca4e2439796",
+    "url": "/hw20_React_Portfolio/static/js/main.aad3087e.chunk.js"
+  },
+  {
+    "revision": "dd1267a54e40e7095474",
+    "url": "/hw20_React_Portfolio/static/js/runtime-main.33b07de2.js"
+  },
+  {
+    "revision": "256f77046c70fe27f56c4b1722467de8",
+    "url": "/hw20_React_Portfolio/static/media/Web-Dev-BG-5.256f7704.png"
+  },
+  {
+    "revision": "bf71fa9de7da5cc12e03a0a5c6269f0b",
+    "url": "/hw20_React_Portfolio/static/media/aaron-profile-drk.bf71fa9d.png"
+  },
+  {
+    "revision": "aa419bca49cfcab6fbd31e365676bc3a",
+    "url": "/hw20_React_Portfolio/static/media/aaron-thomas-logo-NO-WORDS.aa419bca.png"
+  },
+  {
+    "revision": "406851985c186d335238e58d88f2ccb0",
+    "url": "/hw20_React_Portfolio/static/media/password.40685198.png"
+  }
+]);
