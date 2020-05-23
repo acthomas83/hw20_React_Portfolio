bced1a8dd8c174f82329d97e8ebe5eccb53b6f95
Aaron Thomas <acthomas83@gmail.com>
2020-05-22 22:17:10 -0400

    push

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 //port, start server, connect to database, static, routes, middleware
 const express = require("express");
-const mongoose = require("mongoose");
-const routes = require("./routes");
+// const mongoose = require("mongoose");
+// const routes = require("./routes");
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+// const app = express();
+// app.set('port', process.env.PORT || 3000);
 
+// var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+// use port 3000 un less there exists a preconfigured port
+var PORT = process.env.port || 3000;
+
+app.listen(PORT);
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +25,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.use(routes);
+// app.use(routes);
 
 // Start the API server
 app.listen(PORT, function() {
